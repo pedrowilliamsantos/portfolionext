201f1aff6a5dd6683eff1d926499383c87b74ea4
pedrowilliamsantos <pedrowilliam866@gmail.com>
2024-05-06 19:34:28 -0300

    add about page

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import TransitionProvider from "@/components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Create Next App",
+  title: "Pedro William",
   description: "Animated Portfolio Page",
 };
 
